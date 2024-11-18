b6a12187a2fe042c79bd90c888bbe743fbf45af1
KateHolt <kateryna.holtvianska@gmail.com>
2024-11-18 22:22:32 +0200

    Created State service and State repository

--- todolist/src/main/java/com/softserve/itacademy/repository/StateRepository.java
+++ todolist/src/main/java/com/softserve/itacademy/repository/StateRepository.java
@@ -2,6 +2,8 @@ package com.softserve.itacademy.repository;
 
 import com.softserve.itacademy.model.State;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,4 +17,9 @@ public interface StateRepository extends JpaRepository<State, Long> {
     // Method for retrieving a State by name
     // Method for retrieving all State objects sorted by name
 
+    @Query(value = "SELECT id, name, tasks FROM State WHERE State.name=:stateName")
+    State getByName(@Param("stateName") String stateName);
+
+    @Query(value = "SELECT id, name, tasks FROM State ORDER BY name")
+    List<State> getAllStatesSorted();
 }
--- todolist/src/main/java/com/softserve/itacademy/service/impl/StateServiceImpl.java
+++ todolist/src/main/java/com/softserve/itacademy/service/impl/StateServiceImpl.java
@@ -1,45 +1,63 @@
 package com.softserve.itacademy.service.impl;
 
 import com.softserve.itacademy.model.State;
+import com.softserve.itacademy.repository.StateRepository;
 import com.softserve.itacademy.service.StateService;
+import jakarta.persistence.EntityExistsException;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import java.util.Collections;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class StateServiceImpl implements StateService {
+
+    private final StateRepository stateRepository;
 
     @Override
     public State create(State state) {
-
-        return null;
+        if(state != null){
+            if(stateRepository.existsById(state.getId())){
+                throw new EntityExistsException("State with id " + state.getId() + " already exists!");
+            }
+            return stateRepository.save(state);
+        }
+        else throw new IllegalArgumentException("State cannot be null!");
     }
 
     @Override
     public State readById(long id) {
-
-        return null;
+        return stateRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("State with id " + id + " not found!"));
     }
 
     @Override
     public State update(State state) {
-
-        return null;
+        if(state != null){
+            if(stateRepository.existsById(state.getId())){
+                return stateRepository.save(state);
+            }
+            else throw new EntityExistsException("State with id " + state.getId() + " not found!");
+        }
+        else throw new IllegalArgumentException("State cannot be null!");
     }
 
     @Override
     public void delete(long id) {
-
+        if(stateRepository.existsById(id)){
+            stateRepository.deleteById(id);
+        }
+        else throw new EntityExistsException("State with id " + id + " not found!");
     }
 
     @Override
     public List<State> getAll() {
-
-        return Collections.emptyList();
+        return stateRepository.getAllStatesSorted();
     }
 
     @Override
     public State getByName(String name) {
-        return null;
+        return stateRepository.getByName(name);
     }
 }
