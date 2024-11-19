8dd0e60f91d03f1f5685515146b3bda93b0aa33a
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2024-11-19 19:29:40 +0100

    Refactored code

--- todolist/src/main/java/com/softserve/itacademy/service/impl/ToDoServiceImpl.java
+++ todolist/src/main/java/com/softserve/itacademy/service/impl/ToDoServiceImpl.java
@@ -1,10 +1,11 @@
 package com.softserve.itacademy.service.impl;
 
+import com.softserve.itacademy.exception.ToDoAlreadyExistsException;
+import com.softserve.itacademy.exception.ToDoNotFoundException;
 import com.softserve.itacademy.model.ToDo;
 import com.softserve.itacademy.repository.ToDoRepository;
 import com.softserve.itacademy.service.ToDoService;
-import com.softserve.itacademy.service.exception.todoExceptions.ToDoAlreadyExistsException;
-import com.softserve.itacademy.service.exception.todoExceptions.ToDoNotFoundException;
+import com.softserve.itacademy.utility.DataVerification;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -16,8 +17,7 @@ import java.util.logging.Logger;
 
 /**
  * Implementation of the ToDoService interface.
- * Provides business logic for managing ToDo tasks, including creating, updating, deleting,
- * and retrieving tasks.
+ * Provides business logic for managing ToDo tasks, including creating, updating, deleting, and retrieving tasks.
  */
 @Slf4j
 @Service
@@ -27,11 +27,13 @@ public class ToDoServiceImpl implements ToDoService {
     private final ToDoRepository repository;
     private final Logger logger = Logger.getLogger(ToDoServiceImpl.class.getName());
 
+
     @Override
     public ToDo create(ToDo todo) {
         Objects.requireNonNull(todo, "Todo can not be a null");
+        DataVerification.checkToDoValidation(todo);
 
-        if (todo.getId() != 0 && repository.existsById(todo.getId())) {
+        if (repository.existsById(todo.getId())) {
             String errorMessage = "Todo with ID " + todo.getId() + " already exists in the database.";
             logger.log(Level.WARNING, errorMessage);
             throw new ToDoAlreadyExistsException(errorMessage);
@@ -51,26 +53,18 @@ public class ToDoServiceImpl implements ToDoService {
     @Override
     public ToDo update(ToDo todo) {
         Objects.requireNonNull(todo, "Todo cannot be null");
+        DataVerification.checkToDoValidation(todo);
 
-        if (! repository.existsById(todo.getId())) {
-            String errorMessage = "Todo with ID " + todo.getId() + " was not found in the database.";
-            logger.log(Level.WARNING, errorMessage);
-            throw new ToDoNotFoundException(errorMessage);
-        }
+        ToDo toDo = readById(todo.getId());
 
-        ToDo updatedTodo = repository.save(todo);
-        logger.log(Level.INFO, "Todo '{}' was updated successfully", updatedTodo.getTitle());
-        return updatedTodo;
+        repository.save(toDo);
+        logger.log(Level.INFO, "Todo '{}' was updated successfully", toDo.getTitle());
+        return toDo;
     }
 
     @Override
     public void delete(long id) {
-        if (! repository.existsById(id)) {
-            String errorMessage = "Todo with ID " + id + " was not found in the database.";
-            logger.log(Level.WARNING, errorMessage);
-            throw new ToDoNotFoundException(errorMessage);
-        }
-
+        readById(id);
         repository.deleteById(id);
         logger.log(Level.INFO, "Todo with ID '{}' was successfully deleted from the database", id);
     }
@@ -84,4 +78,5 @@ public class ToDoServiceImpl implements ToDoService {
     public List<ToDo> getByUserId(long userId) {
         return repository.getAllToDoByUserId(userId);
     }
+
 }
