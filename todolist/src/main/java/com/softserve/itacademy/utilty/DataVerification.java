fbbe591c6a8be29df58b1404e1c39ef826587ab6
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2024-11-20 02:15:10 +0100

    Fixed problems

--- todolist/src/main/java/com/softserve/itacademy/utilty/DataVerification.java
+++ todolist/src/main/java/com/softserve/itacademy/utilty/DataVerification.java
@@ -8,21 +8,26 @@ import java.time.LocalDateTime;
 public class DataVerification {
 
     public static void checkWhetherToDoIsValidOrNot(ToDo toDo) {
-        if (toDo.getTitle().isBlank()) throw new IllegalArgumentException("Invalid ToDo data: title cannot be blank");
+        if (toDo.getTitle().isBlank()) throw new IllegalArgumentException("Title cannot be blank");
+        if (toDo.getOwner() == null) throw new IllegalArgumentException("Owner cannot be null");
         if (toDo.getCreatedAt().isAfter(LocalDateTime.now()))
-            throw new IllegalArgumentException("Invalid ToDo data: createdAt cannot be in the future or null");
-        if (toDo.getOwner() == null) throw new IllegalArgumentException("Invalid ToDo data: owner cannot be null");
+            throw new IllegalArgumentException("CreatedAt cannot be in the future");
     }
 
     public static void checkUserIsValidOrNot(User user) {
         if (user == null) throw new IllegalArgumentException("User cannot be 'null'");
         if (user.getFirstName().isBlank()) throw new IllegalArgumentException("User first name cannot be empty");
         if (user.getLastName().isBlank()) throw new IllegalArgumentException("User last name cannot be empty");
-        if (!user.getEmail().matches("[\\w-.]+@([\\w-]+\\.)+[\\w-]{2,4}"))
-            throw new IllegalArgumentException("Must be a valid e-mail addres");
         if (user.getRole() == null) throw new IllegalArgumentException("User role can not be null");
-        if (!user.getPassword().matches("(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d!@#$%^&*()_+\\-=]{8,}"))
+
+        if (! user.getEmail().matches("[\\w-.]+@([\\w-]+\\.)+[\\w-]{2,4}"))
+            throw new IllegalArgumentException("Must be a valid e-mail address");
+
+        if (! user.getPassword().matches("(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d!@#$%^&*()_+\\-=]{8,}")) {
             throw new IllegalArgumentException("Must be a valid password");
+        }
+
+
     }
 
 }
